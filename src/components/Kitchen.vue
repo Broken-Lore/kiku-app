02784596b206661fe66b3ce3078a3c2cffaf8628
blaucomfuig <scorcuera6@gmail.com>
2021-07-13 09:57:46 +0200

    create kitchen vue

---
+++ src/components/Kitchen.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="big-container">
+        <img class="img-kitchen" src="../assets/img/vistaCocina.png" alt="">
+        <button class="btn-play">play</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+    .big-container{
+        position:relative;
+        width: 100vw;
+        height: 100vh;
+    }
+
+    .img-kitchen{
+        width: 100vw;
+        height: auto;
+    }
+
+    .btn-play{
+        width: 4rem;
+        height: 4rem;
+        border: none;
+        background-color: red;
+        z-index: 2;
+        position: absolute;
+        bottom: 10%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Kitchen from '../components/Kitchen.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/kitchen',
+    name: 'Kitchen',
+    component: Kitchen
   },
   {
     path: '/about',
