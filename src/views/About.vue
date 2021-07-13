f8b9b70bcef225a23731835fe329377e1a47fa1d
anya_xo <eiekseau@gmail.com>
2021-07-13 12:46:21 +0200

    cat button and vue/sound package

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
-    <h1>humor sapiens eheh</h1>
+    <div class="flex justify-center">
+      <Button
+        @click="play"
+        @mouseleave="stop"
+        label="Trumpet"
+      >
+        🐈
+      </Button>
+    </div>
   </div>
 </template>
+
+<script>
+import { useSound } from "@vueuse/sound";
+import catSfx from "../assets/cat.wav";
+import Button from "../components/Button.vue";
+
+export default {
+  components: {
+    Button,
+  },
+  setup() {
+    const { play, stop } = useSound(catSfx);
+
+    return {
+      play,
+      stop,
+    };
+  },
+};
+</script>
