0c120c1c31404604bf8871c818b3ff91aa613cc7
Jael Masllorens <jmasllorens@gmail.com>
2021-09-07 13:51:40 +0200

    Mutation set_token in auth

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,16 +3,34 @@ import axios from 'axios'
 export default {
     namespaced: true,
     state: {
-        token: null,
+        access_token: null,
         userInfo: null
     },
     mutations: {
+      SET_TOKEN (state, access_token) {
+        state.access_token = access_token
+        }
     },
     actions: {
-        async signIn(_, credentials) {
+        async signIn({ dispatch }, credentials) {
             let response = await axios.post('http://127.0.0.1:8000/api/auth/login', credentials)
 
-            console.log(response.data);
+            dispatch('attempt', response.data.access_token)
+            console.log(response.data.access_token)
+           
+        },
+
+        async attempt({ commit }, access_token) {
+
+           commit('SET_TOKEN', access_token)
+           console.log(access_token)
+
+          /*  try {
+               let response = axios.get('http://127.0.0.1:8000/api/auth/userInfo')
+
+           } catch (e) {
+
+           } */
         }
     }
 }
