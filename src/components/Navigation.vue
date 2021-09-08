8ae23c83ee4c829d9a580d50f9b3a02a3ad9ff7c
Andrea Suarez <asuareztamayo@hotmail.com>
2021-09-08 14:20:24 +0200

    add navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,68 +1,86 @@
 <template>
-   <ul>
-       <li>
-           <router-link 
-           :to="{
-               name: 'Home'
-               }">
-           Home</router-link>
-       </li>
-       
-       <template v-if="authenticated">
-       <li>  {{ user.name }} </li>
-       <li>
-           <router-link 
-           :to="{
-               name: 'Dashboard'
-               }">
-           Dashboard</router-link>
-       </li>
-       <li>
-           <a href="#" @click.prevent="signOut">
-           Sign Out
-           </a>
-       </li>
-       </template>
-       <template v-else>
- <li>
-           <router-link 
-           :to="{
-               name: 'Register'
-               }">
-           Register</router-link>
-       </li>
-        <li>
-           <router-link 
-           :to="{
-               name: 'SignIn'
-               }">
-           Sign In</router-link>
-       </li>
-       </template>
-   </ul>
+<div>
+  <ul class="navbar">
+    <li>
+      <router-link
+        :to="{
+          name: 'Home',
+        }"
+      >
+        Home</router-link
+      >
+    </li>
+
+    <template v-if="authenticated">
+      <li>{{ user.name }}</li>
+      <li>
+        <router-link
+          :to="{
+            name: 'Dashboard',
+          }"
+        >
+          Dashboard</router-link
+        >
+      </li>
+      <li>
+        <a href="#" @click.prevent="signOut">
+          Sign Out
+        </a>
+      </li>
+    </template>
+    <template v-else>
+      <li>
+        <router-link
+          :to="{
+            name: 'Register',
+          }"
+        >
+          Register</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          :to="{
+            name: 'SignIn',
+          }"
+        >
+          Sign In</router-link
+        >
+      </li>
+    </template>
+  </ul>
+  </div>
 </template>
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 export default {
-    computed: {
-        ...mapGetters({
-            authenticated: 'auth/authenticated',
-            user: 'auth/user'
-        })
+  computed: {
+    ...mapGetters({
+      authenticated: "auth/authenticated",
+      user: "auth/user",
+    }),
+  },
+  methods: {
+    ...mapActions({
+      signOutAction: "auth/signOut",
+    }),
+    signOut() {
+      this.signOutAction().then(() => {
+        this.$router.replace({
+          name: "Home",
+        });
+      });
     },
-    methods: {
-        ...mapActions({
-            signOutAction: 'auth/signOut'
-        }),
-        signOut () {
-            this.signOutAction().then(() => {
-                this.$router.replace({
-                    name: 'Home'
-                })
-            })
-        }
-
-    }
-
-}
+  },
+};
 </script>
+
+<style scoped>
+.navbar{
+    font-family: 'Amatic SC', cursive;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    
+}
+</style>
